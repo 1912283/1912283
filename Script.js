3d115d1f685412e7260f005934085312c1f962d1
Rodrick Chinua Ibe-ani <67706024+1912283@users.noreply.github.com>
2024-10-13 09:10:15 +0100

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function (){
+    <button>type"Click me" 
+        Onclick="document.getElementById"("name", "Age", "Grade")
+        .innerHTL = ("name", "Age", "Grade")
+    </button>
+
+})
